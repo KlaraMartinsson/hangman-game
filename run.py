0c8bf1621269c6d5001d68fd8eeb8d4ed02ae66f
KlaraMartinsson <>
2023-09-12 20:46:03 +0000

    Add hangman  class that holds details to create and play game

--- run.py
+++ run.py
@@ -4,9 +4,21 @@ from words import words_2
 from words import words_3
 from hangman_stages import hangman
 
+class Hangman:
+    """
+    Class that holds details to create and play game.
+    """
+    def __init__(self, name, word):
+        self.name = name
+        self.word = word
+        self.guesses = []
+        self.tries = 6
+
+"""
 def get_random_word():
     word = random.choice(list_of_words) #Get's a random word in uppercase
     return word.upper()
+"""
 
 def main():
     print("Welcome player!")
