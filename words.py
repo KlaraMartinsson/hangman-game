4eb5953b37201f1cb30ea669f4a2680597eebb5d
KlaraMartinsson <>
2023-09-12 20:02:48 +0000

    Add levels of difficulty with more words in lists

--- words.py
+++ words.py
@@ -1,1 +1,3 @@
-list_of_words = ['human','computer','car','candy','love','apartment']
+words_1 = ['human','computer','car','candy','love','apartment', 'flight', 'outfit', 'date', 'house', 'couple', 'visit', 'peace']
+words_2 = ['beautiful', 'reveal', 'behavior', 'therapist','immune', 'location', 'criticism', 'bottom', 'private', 'combine']
+words_3 = ['agriculture', 'rehabilitation', 'expression', 'dependence', 'psychology', 'offensive', 'automatic', 'retirement']
