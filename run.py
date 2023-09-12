1e158e31b23f78c82f4238cff75744b7c281b315
KlaraMartinsson <>
2023-09-12 20:42:05 +0000

    Add initial message to player and asking for name

--- run.py
+++ run.py
@@ -7,3 +7,10 @@ from hangman_stages import hangman
 def get_random_word():
     word = random.choice(list_of_words) #Get's a random word in uppercase
     return word.upper()
+
+def main():
+    print("Welcome player!")
+    name = input("Enter your name: ").capitalize()
+    print(name)
+
+main()
